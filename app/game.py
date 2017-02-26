811cad896d7e9417a7ea9de3f7bd4da3c55925c1
Tyler Makaro <tyler@makaro.com>
2017-02-25 19:08:11 -0800

    Added additional tests to test_game.py
    Additionally fixed some bugs that were found in this process

--- app/game.py
+++ app/game.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 class Game():
-    num_instances = 0
     def __init__(self, height, width, game_id):
         self.width = width
         self.height = height
@@ -9,6 +8,7 @@ class Game():
         
         self.board = np.zeros((2,height,width))
         self.snakes = []
+        self.food = []
         
     def parse_board_data(self, data):
         self.snakes = []
@@ -17,21 +17,23 @@ class Game():
             if snake["id"] == data["you"]:
                 self.snakes.insert(0,snake)
             else:
-                self.append(snakes)
+                self.snakes.append(snake)
         
         #0 empty, 1 head, 2 body, 3 tail, 4 food, 5 dead_snake
-        for snake_num, snake in enumerate(snakes):
+        for snake_num, snake in enumerate(self.snakes):
             for x,y in snake["coords"]:
                 self.board[0,y,x] = 2
                 self.board[1,y,x] = snake_num
             head = snake["coords"][0]
             tail = snake["coords"][-1]
-            self.board[0,head[1],head[0]] = 1 
             self.board[0,tail[1],tail[0]] = 3
+            self.board[0,head[1],head[0]] = 1 
         for food in data["food"]:
-            self.board[0,food[1],food[2],] = 4
+            self.board[0,food[1],food[0],] = 4
+        self.food = data["food"]
         for dead_snake in data["dead_snakes"]:
-            self.board[0,y,x] = 5
+            for x,y in dead_snake["coords"]:
+                self.board[0,y,x] = 5
         
     def move():
         pass
