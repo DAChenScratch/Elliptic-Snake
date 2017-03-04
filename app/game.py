9e86cb01bbc124c0ab4af763b23bfb667a6147bc
Tyler Makaro <tyler@makaro.com>
2017-03-04 10:17:26 -0800

    Added priority finding

--- app/game.py
+++ app/game.py
@@ -35,5 +35,45 @@ class Game():
             for x,y in dead_snake["coords"]:
                 self.board[0,y,x] = 5
         
-    def move():
-        pass
+    def move(self):
+        targets = self.priority_foods()
+        board = self.parse_board(self.board)
+        min_path = float('inf')
+        best_path = []
+        for target in targets:
+            path = somewhere.find_path(board, self.snake["coords"][0], target)
+            if len(path) < min_path:
+                best_path = path
+        return best_path[0]
+    
+    def priority_foods(self):
+        targets = [] #if empty is returned then no food is available
+        for food in self.food:
+            min_snake = min([dist(snake["coords"][0],food) for snake in self.snakes[1:]])
+            our_dist = dist(self.snakes[0]["coords[0]"],food)
+            if our_dist <= min_snake:
+                targets.insert(0,food)
+        return targets
+        
+    def parse_board(self):
+        board = np.zeros((2,height,width), dtype=int)
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.board[0,y,x] in [1,2,5]:
+                    board[y,x] = 1
+        for snake in self.snakes[1:]:
+            head = snake["coords"][0]
+            for x,y in [(1,0),(-1,0),(0,1),(0,-1)]:
+                x += head[0]
+                y += head[1]
+                if x >= 0 and x < self.width and y >= 0 and y < self.height:
+                    if len(snake["coords"]) >= len(self.snake["coords"]):
+                        board[y,x] = 1
+                    if self.board[y,x] == 4 or snake["health"] > 97:
+                        tail = snake["coords"][-1]
+                        board[tail[1],tail[0]] = 1
+        return board
+    
+    @staticmethod
+    def dist(start, end):
+        return abs(start[0]-end[0])+abs(start[1]-end[1])
