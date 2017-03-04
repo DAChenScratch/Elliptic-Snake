c789d7e7f77fe95ae6c7eb8b7180fbe8779d725a
Tyler Makaro <tyler@makaro.com>
2017-03-04 13:52:14 -0800

    data parsing should be complete

--- app/game.py
+++ app/game.py
@@ -6,45 +6,35 @@ class Game():
         self.height = height
         self.id = game_id
         
-        self.board = np.zeros((2,height,width), dtype=int)
+        self.board = np.zeros((height,width), dtype=int)
+        self.board_e = np.zeros((height,width), dtype=int)
         self.snakes = []
         self.food = []
         
     def parse_board_data(self, data):
         self.snakes = []
-        self.board = np.zeros((2,self.height,self.width), dtype=int)
+        self.board = np.zeros((self.height,self.width), dtype=int)
         for snake in data["snakes"]:
             if snake["id"] == data["you"]:
                 self.snakes.insert(0,snake)
             else:
                 self.snakes.append(snake)
         
-        #0 empty, 1 head, 2 body, 3 tail, 4 food, 5 dead_snake
-        for snake_num, snake in enumerate(self.snakes):
-            for x,y in snake["coords"]:
-                self.board[0,y,x] = 2
-                self.board[1,y,x] = snake_num
-            head = snake["coords"][0]
-            tail = snake["coords"][-1]
-            self.board[0,tail[1],tail[0]] = 3
-            self.board[0,head[1],head[0]] = 1 
-        for food in data["food"]:
-            self.board[0,food[1],food[0],] = 4
+        self.board = self.parse_board(True)
+        self.board_e = self.parse_board(False)
         self.food = data["food"]
-        for dead_snake in data["dead_snakes"]:
-            for x,y in dead_snake["coords"]:
-                self.board[0,y,x] = 5
         
     def move(self):
-        targets = self.priority_foods()
+        """targets = self.priority_foods()
         board = self.parse_board(self.board)
         min_path = float('inf')
         best_path = []
         for target in targets:
-            path = somewhere.find_path(board, self.snake["coords"][0], target)
+            #path = somewhere.find_path(board, self.snake["coords"][0], target)
             if len(path) < min_path:
                 best_path = path
-        return best_path[0]
+        return best_path[0]"""
+        return "right"
     
     def priority_foods(self):
         targets = [] #if empty is returned then no food is available
@@ -55,23 +45,24 @@ class Game():
                 targets.insert(0,food)
         return targets
         
-    def parse_board(self):
-        board = np.zeros((2,height,width), dtype=int)
-        for x in range(self.width):
-            for y in range(self.height):
-                if self.board[0,y,x] in [1,2,5]:
-                    board[y,x] = 1
-        for snake in self.snakes[1:]:
-            head = snake["coords"][0]
-            for x,y in [(1,0),(-1,0),(0,1),(0,-1)]:
-                x += head[0]
-                y += head[1]
-                if x >= 0 and x < self.width and y >= 0 and y < self.height:
-                    if len(snake["coords"]) >= len(self.snake["coords"]):
-                        board[y,x] = 1
-                    if self.board[y,x] == 4 or snake["health"] > 97:
-                        tail = snake["coords"][-1]
-                        board[tail[1],tail[0]] = 1
+    def parse_board(self, is_us=True):
+        board = np.zeros((self.height,self.width), dtype=int)
+        for snake in self.snakes:
+            for x,y in snake["coords"]:
+                board[y,x] = 1
+            tail = snake["coords"][-1]
+            if snake["health_points"] > 98:
+                board[tail[1],tail[0]] = 1
+
+        if is_us:
+            length = len(self.snakes[0]["coords"])
+            for snake in self.snakes[1:]:
+                head = snake["coords"][0]
+                if length <= len(snake["coords"]):
+                    for x,y in [(1,0),(-1,0),(0,1),(0,-1)]:
+                        x,y = x+head[0], y+head[1]
+                        if x >= 0 and x < self.width and y >= 0 and y < self.height:
+                            board[y,x] = 1 
         return board
     
     @staticmethod
--- tests/test_game.py
+++ tests/test_game.py
@@ -24,11 +24,11 @@ data = {
         ],
         [
           2,
-          1
+          2
         ],
         [
           2,
-          1
+          3
         ]
       ]
     },
@@ -93,22 +93,23 @@ def test_parse_data():
     game1 = Game(5,5,"870d6d79-93bf-4941-8d9e-944bee131167")
     game1.parse_board_data(data)
     assert game1.food == [[3,3],[0,4]]
+    assert game1.snakes[1]["coords"][0] == [2,1]
     assert game1.snakes == [{"taunt": "git gud","name": "my-snake","id": "25229082-f0d7-4315-8c52-6b0ff23fb1fb",
         "health_points": 93,"coords": [[0,0],[0,0],[0,0]]},
         {"taunt": "gotta go fast","name": "other-snake","id": "0fd33b05-37dd-419e-b44f-af9936a0a00c","health_points": 50,
-         "coords": [[2,1],[2,1],[2,1]]}]
-    expected_board = np.array([[[ 1,  0,  0,  0,  0,],
-        [ 0,  0,  1,  0,  5],
-        [ 0,  0,  0,  0,  5],
-        [ 0,  0,  0,  4,  5],
-        [ 4,  0,  0,  0,  0]],
-
-        [[ 0,  0,  0,  0,  0],
+         "coords": [[2,1],[2,2],[2,3]]}]
+    expected_board = np.array([[ 1,  0,  1,  0,  0,],
+        [ 0,  1,  1,  1,  0],
         [ 0,  0,  1,  0,  0],
-        [ 0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0]]])
+        [ 0,  0,  1,  0,  0],
+        [ 0,  0,  0,  0,  0]])  
+    expected_board_e = np.array([[ 1,  0,  0,  0,  0,],
+        [ 0,  0,  1,  0,  0],
+        [ 0,  0,  1,  0,  0],
+        [ 0,  0,  1,  0,  0],
+        [ 0,  0,  0,  0,  0]])
     assert np.array_equal(expected_board, game1.board)
+    assert np.array_equal(expected_board_e, game1.board_e)
     print(game1.board)
 
 if __name__ == "__main__":
